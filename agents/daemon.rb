f2595d002c84dc55ef80684d01ae8230a74842af
Matt Zukowski <matt@zukowski.ca>
2011-06-08 01:19:03 -0400

    - Inquisitor should now be able to handle the groupwork part of the curnit (but homework may now be broken -- haven't tested properly) - config values are now read from config.rb file

--- agents/daemon.rb
+++ agents/daemon.rb
@@ -1,20 +1,17 @@
 $: << "sail.rb/lib"
 require 'sail/daemon'
 
+load 'config.rb'
+
 @daemon = Sail::Daemon.spawn(
   :name => "dalite-physics",
   :path => '.',
   :verbose => true
 )
 
-#RUN_ID = 2
-RUN_ID = 6
-ARLO_URL = "http://arlo.proto.encorelab.org"
-#ROLLCALL_URL = "http://rollcall.proto.encorelab.org"
-ROLLCALL_URL = "http://localhost:3000"
-XMPP_DOMAIN = "proto.encorelab.org"
+
 
 require 'inquisitor'
-@daemon << Inquisitor.new(:run_id => RUN_ID)
+@daemon << Inquisitor.new
 
 @daemon.start
